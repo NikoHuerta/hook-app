e24a3e6f828bc32a48bf87ec02547707ece9694f
Nicolas <nicolas.huerta@usach.cl>
2021-11-07 23:44:02 -0300

    test adv

--- src/tests/components/08-useReducer/todoReducer.test.js
+++ src/tests/components/08-useReducer/todoReducer.test.js
@@ -29,6 +29,45 @@ describe('Pruebas en todoReducer', () => {
         expect(state.length).toEqual(demoTodos.length + 1); //igualar tamaño de arreglo
         expect(state).toEqual([...demoTodos, newTodo]);    
     });
+
+    test('Debe de borrar un TODO', () => {
+
+        //action.payload = ID del TODO
+        const action = {
+            type : 'delete',
+            payload: 3
+        }
+
+        const state = todoReducer(demoTodos, action);
+        expect(state.length).toEqual(demoTodos.length - 1);
+        
+        const result_del = demoTodos.filter( todo => todo.id !== action.payload );
+        expect(state).toEqual([...result_del]); 
+
+    });
+
+    test('Debe de hacer el TOGGLE del TODO', () => {
+
+        const action = {
+            type : 'toggle',
+            payload: 3
+        }
+        
+        const state = todoReducer(demoTodos, action);
+
+        const modified = demoTodos.map( todo =>  
+                            (todo.id === action.payload) 
+                            ? { ...todo, done: !todo.done }
+                            : todo
+                            );
+        
+        expect(state[2].done).toBe(true);
+        expect(state).toEqual(modified);
+        
+
+    })
+    
+
     
 
 });
